af3da5b6cb9cf85d182d73641af8f87b4d43c932
vincentwilliamhunt <vincentwilliamhunt@gmail.com>
2018-06-11 18:27:37 -0700

    Formatting corrections.

--- app/src/main/java/com/example/android/musicalstructureapp/AlbumsActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/AlbumsActivity.java
@@ -1,14 +1,11 @@
 package com.example.android.musicalstructureapp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.android.musicalstructureapp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
